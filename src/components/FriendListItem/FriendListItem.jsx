46fddfe4dd3f5c9332543dfe6d42c2e6c6f32f8d
Khalatska <135690966+Khalatska@users.noreply.github.com>
2024-03-11 16:51:57 +0100

    fix css

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -2,23 +2,14 @@ import css from "./FriendListItem.module.css";
 import clsx from "clsx";
 
 const FriendListItem = ({ friend }) => {
-  const isOnlineFriend = friend.isOnline;
-
   return (
     <li>
       <img src={friend.avatar} alt="Avatar" width="48" />
       <p>{friend.name}</p>
       {friend.isOnline ? (
-        <p
-          className={clsx({
-            [css.online]: isOnlineFriend,
-            [css.offline]: !isOnlineFriend,
-          })}
-        >
-          Online
-        </p>
+        <p className={css.online}>Online</p>
       ) : (
-        <p>Offline</p>
+        <p className={css.offline}>Offline</p>
       )}
     </li>
   );
