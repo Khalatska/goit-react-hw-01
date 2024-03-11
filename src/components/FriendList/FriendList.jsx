9a5a3d6fcd97843249041061ab8901bc0b998980
Khalatska <135690966+Khalatska@users.noreply.github.com>
2024-03-11 17:04:45 +0100

    added css

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -2,7 +2,7 @@ import FriendListItem from "../FriendListItem/FriendListItem";
 import css from "./FriendList.module.css";
 const FriendList = ({ friends }) => {
   return (
-    <ul>
+    <ul className={css.friendsList}>
       {Array.isArray(friends) &&
         friends.map((friend) => {
           return <FriendListItem key={friend.id} friend={friend} />;
